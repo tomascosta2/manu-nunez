9faf924516f37e8594e96ea2d545f6a27a7daed0
Tomas Costa <tomascostapp@gmail.com>
2026-01-09 23:43:09 -0300

    d

--- src/app/components/CalificationFormDirect.tsx
+++ src/app/components/CalificationFormDirect.tsx
@@ -349,9 +349,7 @@ export default function CalificationFormDirect({ variant }: Props) {
 
       const isQualified =
         (data.presupuesto === 'presupuesto-intermedio' || data.presupuesto === 'presupuesto-alto') &&
-        (data.edad === 'adulto' || data.edad === 'mayor') &&
-        (data.urgencia === '5' || data.urgencia === '7' || data.urgencia === '10') &&
-        (data.ocupacion === 'negocio-propio' || data.ocupacion === 'profesional' || data.ocupacion === 'freelance' );
+        (data.edad === 'adulto' || data.edad === 'mayor')
 
       localStorage.setItem('isQualified', isQualified ? 'true' : 'false');
       localStorage.setItem('name', data.name);
