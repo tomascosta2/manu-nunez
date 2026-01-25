ed5ec0ec8a7931c1b80d830fe677446f670c3529
Tomas Costa <tomascostapp@gmail.com>
2026-01-25 14:18:38 -0300

    d

--- src/app/components/CalificationFormDirect.tsx
+++ src/app/components/CalificationFormDirect.tsx
@@ -230,9 +230,9 @@ export default function CalificationFormDirect({ variant }: Props) {
           'En caso de ser aceptado y sabiendo que es un servicio integral de 3 meses ¿Cuanto estas dispuesto a invertir en vos, tu salud y tu fisico y ser acompañado ayudandote a lograr tus objetivos de forma garantizada? *',
         options: [
           { value: 'presupuesto-bajo', label: 'Menos de 200 USD (En este caso no vas a poder agendar)' },
-          { value: 'presupuesto-intermedio', label: 'Entre 200 y 400 USD' },
-          { value: 'presupuesto-alto', label: 'Entre 400 y 600 USD' },
-          { value: 'presupuesto-muy-alto', label: '+600 USD' },
+          { value: 'presupuesto-intermedio', label: 'Puedo hacer un pago de 200 y dos cuotas de 100' },
+          { value: 'presupuesto-alto', label: 'Puedo hacer dos pagos de 200' },
+          { value: 'presupuesto-muy-alto', label: 'Puedo hacer un pago de 400' },
         ],
       },
     ],
